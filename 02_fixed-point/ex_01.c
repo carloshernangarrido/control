c23a4e2f254385a0cee55d375ca348716f7d86c9
Hernán <carloshernangarrido@gmail.com>
2023-03-03 12:33:12 -0300

    first commit

---
+++ 02_fixed-point/ex_01.c
@@ -0,0 +1,31 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+
+// Para usar la biblioteca <math.h>, compilar con "-lm", por ejemplo:
+// gcc ex_01.c -o ex_01 -lm
+
+int main(void)
+{	
+	int8_t a, b, c, d, s1, s2;
+	 
+	a = 127;
+	b = 127;
+	
+	c = a + b;	
+	d = a * b;
+	
+	s1 = (-8) >> 2;
+	s2 = (-1) >> 5;
+
+	printf("c = %d \n", c );
+	printf("d = %d \n", d );
+	printf("s1 = %d \n", s1 );
+	printf("s2 = %d \n", s2 );
+	
+	printf("INT_MAX = %d \n" , INT_MAX);
+	printf("INT_MIN = %d \n" , INT_MIN);
+	
+	return 0;
+}
---
+++ README.md
@@ -0,0 +1,6 @@
+
+# Cátedra de Control y Sistemas
+
+## Ingeniería en Mecatrónica, Facultad de Ingeniería, Universidad Nacional de Cuyo
+
+Archivos de la cátedra de Control y Sistemas de la carrera de Mecatrónica, Facultad de Ingeniería, Universidad Nacional de Cuyo.
