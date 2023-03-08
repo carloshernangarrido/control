da234c0c50068839bb27016131f6d9bf898e8271
Hernán <carloshernangarrido@gmail.com>
2023-03-08 16:53:35 -0300

    TP2, Ej 1 solved

---
+++ 02_fixed-point/Resolution example/Ej1.c
@@ -0,0 +1,27 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdint.h>
+
+void main(void)
+{	
+	// signed char a, b, c, d, s1, s2;
+    unsigned char a, b, c;
+    int32_t d; 
+    signed char s1, s2;
+
+	
+	a = 127;
+	b = 127;
+	
+	c = a + b;	
+	// d = a * b;
+	d = (int32_t)(a * b);
+	
+	s1 = (-8) >> 2;
+	s2 = (-1) >> 5;
+
+	printf("c = %d \n", c );
+	printf("d = %d \n", d );
+	printf("s1 = %d \n", s1 );
+	printf("s2 = %d \n", s2 );
+}
