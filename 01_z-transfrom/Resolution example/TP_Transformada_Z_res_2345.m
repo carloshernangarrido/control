a98a06a165611d895fa03bd271ab08fe9718a521
Hernán <carloshernangarrido@gmail.com>
2023-03-08 15:47:58 -0300

    Ej. 3 y 4, TP1

--- 01_z-transfrom/Resolution example/TP_Transformada_Z_res_2345.m
+++ 01_z-transfrom/Resolution example/TP_Transformada_Z_res_2345.m
@@ -1,5 +1,6 @@
 clc
 clear
+close all
 
 % Ej. 2.4
 n  = [0,  1,  2,  3]
@@ -13,8 +14,48 @@ X2 = filt(x2, [1])
 X = X1*X2
 
 % Ej. 3
+oldparam = sympref('HeavisideAtOrigin',1);
 syms n
 x = heaviside(n)*(0.5)^n + heaviside(n)*(0.3)^n + heaviside(n)*(0.9)^n
 X = ztrans(x)
 pretty(X)
-% ROC: |z| > 10/9
+% ROC: |z| > 9/10
+
+% Ej. 4.1
+H = filt([1], [1, -.7]);
+H
+% Ej. 4.2
+% x = dirac(n)
+X = 1
+Y = H * X
+syms z
+disp("response to impluse")
+y = iztrans(1/(1-0.7*z^-1))
+% Ej. 4.3
+% x = heaviside(n)
+X = filt([1], [1, -1])
+Y = H * X
+syms z
+disp("response to step")
+y = iztrans(1/(1 - 1.7*z^-1 + 0.7*z^-2))
+
+% Using residuez
+[r,p,k] = residuez([1], [1, -1.7, 0.7])
+
+% Using filter
+disp("response to impluse")
+n = 0:11;
+x_n = [0 0 0 1 0 0 0 0 0 0 0 0]
+y_n = filter(cell2mat(H.numerator), cell2mat(H.denominator), x_n)
+figure
+hold on
+plot(n, x_n, 'LineStyle', 'none', 'marker', 'o', 'color', 'blue')
+plot(n, y_n, 'LineStyle', 'none', 'marker', 'o', 'color', 'red')
+
+disp("response to step")
+x_n = [0 0 0 1 1 1 1 1 1 1 1 1]
+y_n = filter(cell2mat(H.numerator), cell2mat(H.denominator), x_n)
+figure
+hold on
+plot(n, x_n, 'LineStyle', 'none', 'marker', 'o', 'color', 'blue')
+plot(n, y_n, 'LineStyle', 'none', 'marker', 'o', 'color', 'red')
